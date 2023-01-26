effb8d5996eb08521f1d1b8414adbedba728fcb6
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2023-01-26 16:54:02 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,31 +8,25 @@ app.use(
   })
 )
 
-const STRIPE_PRIVATE_KEY='sk_test_51KZXQ8Foq0qfjkNLwv2SiG7gYVwkpz878gB5KLJndEDiwCs3ECEh8Uv6rioYChiT1SuNrWDGKCTzjXJ40mUyn7wc00oYnGs75r'
+const STRIPE_PRIVATE_KEY = 'sk_test_51KZXQ8Foq0qfjkNLwv2SiG7gYVwkpz878gB5KLJndEDiwCs3ECEh8Uv6rioYChiT1SuNrWDGKCTzjXJ40mUyn7wc00oYnGs75r'
 const CLIENT_URL = 'http://localhost:3000/'
-
 const stripe = require("stripe")(STRIPE_PRIVATE_KEY)
 
-const storeItems = new Map([
-  [1, { priceInCents: 10000, name: "Learn React Today" }],
-  [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-])
-
 app.post("/create-checkout-session", async (req, res) => {
+  let reqItems = req.body.items
+
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
-      line_items: req.body.items.map(item => {
-        const storeItem = storeItems.get(item.id)
-        console.log('storeItem', storeItem)
+      line_items: reqItems.map(item => {
         return {
           price_data: {
             currency: "usd",
             product_data: {
-              name: storeItem.name,
+              name: item.name,
             },
-            unit_amount: storeItem.priceInCents,
+            unit_amount: item.priceInCents,
           },
           quantity: item.quantity,
         }
@@ -40,11 +34,14 @@ app.post("/create-checkout-session", async (req, res) => {
       success_url: `${CLIENT_URL}`,
       cancel_url: `${CLIENT_URL}`,
     })
-    console.log(session)
 
-    res.json({ url: session.url })
+    res.json({
+      url: session.url
+    })
   } catch (e) {
-    res.status(500).json({ error: e.message })
+    res.status(500).json({
+      error: e.message
+    })
   }
 })
 app.listen(5000)
